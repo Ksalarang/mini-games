2e3bc10a3463e46948e8ad63e08198122eab0d2e
Diyar Taikenov <dtaikenov@astrocat.co>
2026-02-09 03:57:41 +0500

    Fix scene loading

--- Assets/Scripts/Core/Services/AddressablesSceneService.cs
+++ Assets/Scripts/Core/Services/AddressablesSceneService.cs
@@ -7,24 +7,14 @@ namespace Core.Services
 {
     public class AddressablesSceneService : ISceneService
     {
-        private SceneInstance? currentScene;
-        
         public async UniTask<SceneInstance> LoadSceneAsync(
             string key,
             LoadSceneMode mode = LoadSceneMode.Single,
             bool activateOnLoad = true)
         {
-            if (currentScene.HasValue && currentScene.Value.Scene.isLoaded)
-            {
-                await Addressables.UnloadSceneAsync(currentScene.Value);
-                
-                currentScene = null;
-            }
-            
             var handle = Addressables.LoadSceneAsync(key, mode, activateOnLoad);
-            await handle.ToUniTask();
+            await handle.Task;
 
-            currentScene = handle.Result;
             return handle.Result;
         }
     }
