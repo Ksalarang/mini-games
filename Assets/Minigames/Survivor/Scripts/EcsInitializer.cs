78e66b52da3b9d3f18f2ecce634076e53d0c8fee
Diyar Taikenov <drtaikenov@gmail.com>
2026-07-14 03:57:09 +0500

    Add EcsInitializer

---
+++ Assets/Minigames/Survivor/Scripts/EcsInitializer.cs
@@ -0,0 +1,17 @@
+﻿using Leopotam.Ecs;
+using UnityEngine;
+
+namespace Minigames.Survivor.Scripts
+{
+    public class EcsInitializer : MonoBehaviour
+    {
+        private EcsWorld world;
+        private EcsSystems systems;
+
+        private void Awake()
+        {
+            world = new EcsWorld();
+            systems = new EcsSystems(world);
+        }
+    }
+}
