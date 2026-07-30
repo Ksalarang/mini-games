ae9d95eee95c62fe45d88d4f0ceb4fb30c6a8fcc
Diyar Taikenov <anbleidd@gmail.com>
2026-07-30 18:22:11 +0500

    Add PlayerPrefsTools

---
+++ Assets/Scripts/Core/Editor/PlayerPrefsTools.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Core.Editor
+{
+    public static class PlayerPrefsTools
+    {
+        [MenuItem("Tools/PlayerPrefs/DeleteAll")]
+        public static void DeleteAll()
+        {
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.Save();
+        }
+    }
+}
